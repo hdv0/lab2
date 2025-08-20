8c5632f56009703f34643d83f85638e4fddd123c
Cynth1a-M <103557838+Cynth1a-M@users.noreply.github.com>
2025-08-20 16:39:27 -0700

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Author: Cynthia Martinez
+// Description: Hello World in C++
+//============================================================
+#include <iostream>
+using namespace std;
+int hello1Function(string name1);
+int hello2Function(string name2);
+int main()
+{
+string name1="Cynthia Martinez";
+hello1Function(name1);
+string name2="Hailey Driscoll";
+hello2Function(name2);
+return 0;
+}
